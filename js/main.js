c3dd6507e7aa2129021040f31c980a81177497a7
Taylor Stib <taylor@raddevelopment.io>
2015-08-15 12:52:00 -0700

    Add passing data

--- js/main.js
+++ js/main.js
@@ -1,46 +1,54 @@
 console.log('main.js loaded');
+d3.json("./data/passing/2014_passing_short.json", function(data){
+  var w = 650;
+  var h = 400;
+  var svg = d3.select("#container").append("svg")
+    .attr("height", h)
+    .attr("width", w);
+  var padding = 30;
+  var barPadding = 1;
+  var offset = 8;
+  var xScale = d3.scale.linear().domain([0, d3.max(data, function(d){return d.YDS; })]).range([0, w]);
+  var yScale = d3.scale.linear().domain([0, d3.max(data, function(d){return d.YDS; })]).range([0, h]);
+  var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+  var yAxis = d3.svg.axis().scale(yScale).orient("left");
 
-var data = [12, 3, 4, 10, 12, 7, 8, 2, 12, 3, 4, 5, 9, 21, 10, 3, 14, 7];
-var svg = d3.select("#container").append("svg")
-  .attr("height", 400)
-  .attr("width", 400);
-var w = 400;
-var h = 300;
-var barPadding = 1;
-var offset = 8;
-var xScale = d3.scale.linear();
+  svg.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", function(d, i) {
+      return i * (w / data.length); //Bar width of 20 plus 1 for padding
+    })
+    .attr("y", function(d) {
+      return h - yScale(d.YDS);
+    })
+    .attr("width", w / data.length - barPadding)
+    .attr("height", function(d) {
+      return yScale(d.YDS);
+    })
+    .attr("fill", function(d) {
+      return "rgb(" + Math.round(d.YDS / 20) + ",0,0)";
+    });
 
-svg.selectAll("rect")
-  .data(data)
-  .enter()
-  .append("rect")
-  .attr("x", function(d, i) {
-    return i * (w / data.length); //Bar width of 20 plus 1 for padding
-  })
-  .attr("y", function(d) {
-    return h - (d * offset);
-  })
-  .attr("width", w / data.length - barPadding)
-  .attr("height", function(d) {
-    return d * offset;
-  })
-  .attr("fill", function(d) {
-    return "rgb(0,0," + d * 14 + ")";
-  });
-
-// Add data value labels
-svg.selectAll("text")
-  .data(data)
-  .enter()
-  .append("text")
-  .text(function(d) {
-    return d * 8;
-  })
-  .attr("x", function(d, i) {
-    return i * (w / data.length) + 4;
-  })
-  .attr("y", function(d) {
-    return h - (d * offset) + 11;
-  })
-  .attr("font-size", "10px")
-  .attr("fill", 'white');
+  // Add data value labels
+  svg.selectAll("text")
+    .data(data)
+    .enter()
+    .append("text")
+    .text(function(d) {
+      return d.YDS;
+    })
+    .attr("x", function(d, i) {
+      return i * (w / data.length) + 4;
+    })
+    .attr("y", function(d) {
+      return h - yScale(d.YDS) + 10;
+    })
+    .attr("font-size", "10px")
+    .attr("fill", 'white');
+    
+  // svg.append("g").attr("class", 'axis').attr("transform", "translate(0,"+h+"").call(xAxis);
+  // svg.append("g").attr("class", 'axis').attr("transform", "translate(18,0)").call(yAxis);
+  console.log('Chart completed');
+});
