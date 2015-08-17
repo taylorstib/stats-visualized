6298c70aeee4194948d2e8297202124d971f01d0
Taylor Stib <taylor@raddevelopment.io>
2015-08-16 19:42:03 -0700

    Add data for steps and a graph

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 console.log('main.js loaded');
 d3.json("./data/passing/2014_passing_short.json", function(data){
-  var w = 650;
-  var h = 400;
+  var w = $("#container").width();
+  var h = 200;
   var svg = d3.select("#container").append("svg")
     .attr("height", h)
     .attr("width", w);
@@ -51,4 +51,52 @@ d3.json("./data/passing/2014_passing_short.json", function(data){
   // svg.append("g").attr("class", 'axis').attr("transform", "translate(0,"+h+"").call(xAxis);
   // svg.append("g").attr("class", 'axis').attr("transform", "translate(18,0)").call(yAxis);
   console.log('Chart completed');
+});
+
+d3.json("./data/steps/steps.json", function(data){
+  console.log(data);
+  var w = $("#container").width();
+  var h = 200;
+  var svg = d3.select("#container").append("svg")
+    .attr("height", h)
+    .attr("width", w);
+  var padding = 30;
+  var barPadding = 1;
+  var offset = 8;
+  var xScale = d3.scale.linear().domain([0, d3.max(data, function(d){return d.steps; })]).range([0, w]);
+  var yScale = d3.scale.linear().domain([0, d3.max(data, function(d){return d.steps; })]).range([0, h]);
+  var good = 0;
+  var okay = 0;
+  var bad  = 0;
+
+  svg.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", function(d, i) {
+      return i * (w / data.length); //Bar width of 20 plus 1 for padding
+    })
+    .attr("y", function(d) {
+      return h - yScale(d.steps);
+    })
+    .attr("width", w / data.length - barPadding)
+    .attr("height", function(d) {
+      return yScale(d.steps);
+    })
+    .attr("fill", function(d) {
+      if (d.steps > 10000) {
+        good += 1;
+        return "rgb(0,200,0)";
+      } else if (d.steps > 5000) {
+        okay += 1;
+        return "rgb(213, 217, 50)";
+      } else {
+        bad += 1;
+        return "rgb(255, 0, 0)";
+      }
+    });
+    
+    console.log("good = " + good);
+    console.log("okay = " + okay);
+    console.log("bad = " + bad);
 });
