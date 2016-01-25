38b1f9e6336ec06d3a9eee26e48afe30e717f802
Taylor Stib <taylor@raddevelopment.io>
2016-01-24 19:21:21 -0700

    Many updates to the JS

--- js/steps.js
+++ js/steps.js
@@ -2,6 +2,11 @@
 var parseDate = d3.time.format("%m/%d/%Y").parse;
 var parseMonth = d3.time.format("%m").parse;
 var dateFormat = d3.time.format("%a %m/%d");
+var milSecPerDay = 864000000;
+
+// set color Variables
+var success_green = "rgba(0,200,0,1)";
+var fail_grey = "rgba(153, 151, 151, 0.7)";
 
 var margin = {top: 10, right: 30, bottom: 30, left: 50},
     width = 1200 - margin.left - margin.right,
@@ -20,7 +25,7 @@ var svg_min = d3.select("#minutes").append("svg")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   
 // MONTHS GRAPH SETUP
-var months_width = 600; months_height = 350;
+var months_width = 600; months_height = 200;
 
 var months_chart = d3.select("#months").append("svg")
   .attr("height", months_height + margin.top + margin.bottom)
@@ -38,7 +43,14 @@ var months_array = [];
 
 // Bar graph of my daily steps
 d3.json("./data/steps/steps.json" + '?' + Math.floor(Math.random() * 1000), function(data){
-  
+
+  var beginningDate = new Date('Dec 1, 2015');
+  console.log(beginningDate);
+  console.log(new Date(data[0].date));
+  var filteredData = data.filter(function(d){
+    return new Date(d.date).getTime() > beginningDate.getTime();
+  });
+
 // MONTHS GRAPH
 
   data.forEach(function(d) { d.dateM = parseMonth(d.date); });
@@ -74,9 +86,9 @@ d3.json("./data/steps/steps.json" + '?' + Math.floor(Math.random() * 1000), func
     .attr("width", months_width / months_array.length - monthBarPadding)
     .attr("height", function(d) { return months_height - mYScale(d); })
     .attr("fill", function(d, i){ if (d < 5500) {
-      return "rgba(111, 111, 111, 0.7)";
+      return fail_grey;
     } else {
-      return "rgba(0, 255, 40, .7)";
+      return success_green;
     } });
   
   months_chart.append("g")
@@ -98,9 +110,9 @@ d3.json("./data/steps/steps.json" + '?' + Math.floor(Math.random() * 1000), func
     .domain(d3.extent(data, function(d) { return d.date; }))
     .range([0, width]);
 
-  
+  console.log(d3.min(data, function(d){ return d.steps;}));
   // Commented yscale uses the data to make it dynamic
-  var yScale = d3.scale.linear().domain([0, d3.max(data, function(d){return d.steps ; })]).range([height, 0]).nice();
+  var yScale = d3.scale.linear().domain([d3.min(data, function(d){ return d.steps;}), d3.max(data, function(d){return d.steps ; })]).range([height, 0]).nice();
   // var yScale = d3.scale.linear().domain([0, 16000]).range([height, 0]);
   
 
@@ -127,15 +139,11 @@ d3.json("./data/steps/steps.json" + '?' + Math.floor(Math.random() * 1000), func
       if (d.steps > 10000) {
         good += 1;
         total += 1;
-        return "rgba(0,200,0,1)";
-      } else if (d.steps > 6000) {
-        okay += 1;
-        total += 1;
-        return "rgba(153, 151, 151, 0.7)";
+        return success_green;
       } else {
         bad += 1;
         total += 1;
-        return "rgba(153, 151, 151, 0.7)";
+        return fail_grey;
       }
     })
     .on('click', function(d) {
@@ -161,7 +169,7 @@ d3.json("./data/steps/steps.json" + '?' + Math.floor(Math.random() * 1000), func
       .call(xAxis);
 
     d3.select("#good").text("good = " + good + " = "+ Math.round(good/total * 100) + "%");
-    d3.select("#okay").text("okay = "+okay + " = "+ Math.round(okay/total * 100) + "%");
+    // d3.select("#okay").text("okay = "+okay + " = "+ Math.round(okay/total * 100) + "%");
     d3.select("#bad").text("bad = "+bad + " = "+ Math.round(bad/total * 100) + "%");
     d3.select("#total").text("Total = "+ total + " = 100%").style({"font-weight": "bold", "border-top": "2px solid black", "padding-top": "8px"});
 
@@ -203,15 +211,15 @@ d3.json("./data/steps/steps.json" + '?' + Math.floor(Math.random() * 1000), func
       if (d.minutes > 75) {
         good_min += 1;
         total_min += 1;
-        return "rgba(0,200,0,.7)";
+        return success_green;
       } else if (d.minutes > 50) {
         okay_min += 1;
         total_min += 1;
-        return "rgba(213, 217, 50,.7)";
+        return fail_grey;
       } else if (d.minutes > 0) {
         bad_min += 1;
         total_min += 1;
-        return "rgba(153, 151, 151, 0.7)";
+        return fail_grey;
       }
     })
     .on('click', function(d) {
@@ -239,7 +247,7 @@ d3.json("./data/steps/steps.json" + '?' + Math.floor(Math.random() * 1000), func
       .call(xMinAxis);
 
     d3.select("#good_min").text("good = " + good_min + " = "+ Math.round(good_min/total_min * 100) + "%");
-    d3.select("#okay_min").text("okay = "+okay_min + " = "+ Math.round(okay_min/total_min * 100) + "%");
+    // d3.select("#okay_min").text("okay = "+okay_min + " = "+ Math.round(okay_min/total_min * 100) + "%");
     d3.select("#bad_min").text("bad = "+bad_min + " = "+ Math.round(bad_min/total_min * 100) + "%");
     d3.select("#total_min").text("Total = "+ total_min + " = 100%").style({"font-weight": "bold", "border-top": "2px solid black", "padding-top": "8px"});
 
@@ -305,11 +313,11 @@ function renderMonth(month){
         .attr("height", function(d) { return height - yScale(d.steps); })
         .attr("fill", function(d) {
           if (d.steps > 10000) {
-            return "rgba(0,200,0,0.7)";
+            return success_green;
           } else if (d.steps > 6000) {
-            return "rgba(153, 151, 151, 0.7)";
+            return fail_grey;
           } else {
-            return "rgba(153, 151, 151, 0.7)";
+            return fail_grey;
           }
         })
         .attr("stroke", function(d) {return '#eee';});
@@ -322,11 +330,11 @@ function renderMonth(month){
         .attr("height", function(d) { return height - yScale(d.steps); })
         .attr("fill", function(d) {
           if (d.steps > 10000) {
-            return "rgba(0,200,0,0.7)";
+            return success_green;
           } else if (d.steps > 6000) {
-            return "rgba(153, 151, 151, 0.7)";
+            return fail_grey;
           } else {
-            return "rgba(153, 151, 151, 0.7)";
+            return fail_grey;
           }
         })
         .on('click', function(d){
@@ -353,11 +361,11 @@ function renderMonth(month){
         .attr("height", function(d) { return height - yMinScale(d.minutes); })
         .attr("fill", function(d) {
           if (d.minutes > 75) {
-            return "rgba(0,200,0,0.7)";
+            return success_green;
           } else if (d.minutes > 50) {
-            return "rgba(213, 217, 50,0.7)";
+            return fail_grey;
           } else {
-            return "rgba(153, 151, 151, 0.7)";
+            return fail_grey;
           }
         })
         .attr("stroke", function(d) {return '#eee';});
@@ -370,11 +378,11 @@ function renderMonth(month){
         .attr("height", function(d) { return height - yMinScale(d.minutes); })
         .attr("fill", function(d) {
           if (d.minutes > 75) {
-            return "rgba(0,200,0,0.7)";
+            return success_green;
           } else if (d.minutes > 50) {
-            return "rgba(213, 217, 50,0.7)";
+            return fail_grey;
           } else {
-            return "rgba(153, 151, 151, 0.7)";
+            return fail_grey;
           }
         })
         .on('click', function(d){
@@ -408,10 +416,7 @@ function renderMonth(month){
         else if (d.steps > 10000) {
           good += 1;
           total += 1;
-        } else if (d.steps > 6000) {
-          okay += 1;
-          total += 1;
-        } else if (d.steps < 6000) {
+        } else {
           bad += 1;
           total += 1;
         }
@@ -423,22 +428,19 @@ function renderMonth(month){
          else if (d.minutes > 75) {
            good_min += 1;
            total_min += 1;
-         } else if (d.minutes > 50) {
-           okay_min+= 1;
-           total_min+= 1;
          } else {
            bad_min+= 1;
            total_min += 1;
          }
         });
         d3.select("#good").text("good = " + good + " = "+ Math.round(good/total * 100) + "%");
-        d3.select("#okay").text("okay = "+okay + " = "+ Math.round(okay/total * 100) + "%");
+        // d3.select("#okay").text("okay = "+okay + " = "+ Math.round(okay/total * 100) + "%");
         d3.select("#bad").text("bad = "+bad + " = "+ Math.round(bad/total * 100) + "%");
         d3.select("#total").text("Total = "+ total + " = 100%").style({"font-weight": "bold", "border-top": "2px solid black", "padding-top": "8px"});
         d3.select("#step-pie").text(good + "," + okay + "," + bad);
         
         d3.select("#good_min").text("good = " + good_min + " = "+ Math.round(good_min/total_min * 100) + "%");
-        d3.select("#okay_min").text("okay = "+okay_min + " = "+ Math.round(okay_min/total_min * 100) + "%");
+        // d3.select("#okay_min").text("okay = "+okay_min + " = "+ Math.round(okay_min/total_min * 100) + "%");
         d3.select("#bad_min").text("bad = "+bad_min + " = "+ Math.round(bad_min/total_min * 100) + "%");
         d3.select("#total_min").text("Total = "+ total_min + " = 100%").style({"font-weight": "bold", "border-top": "2px solid black", "padding-top": "8px"});
         d3.select("#min-pie").text(good_min + "," + okay_min + "," + bad_min);
